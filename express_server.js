a1fa8aff674d3797d81858b7defade2468f38546
Karen <to.karenngo@gmail.com>
2021-12-07 12:11:34 -0500

    added notes and added app.get with res.redirect to long url so when clicked on the link it will redirect tto the long url that was stored with a previous route

--- express_server.js
+++ express_server.js
@@ -30,25 +30,39 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
+  const templateVars = {urls: urlDatabase}; // ASK MENTOR- urlDatabase is already an object.. why must be put into another object?
+  res.render("urls_index", templateVars); // this is passing in our database "urlDatabse" to render
+});
+
+app.get("/u/:shortURL", (req, res) => {  // ":" variable/parameter in a URL, since it's a .get, you are requesting so the parameters you want to access are in req
+  const longURL = urlDatabase[req.params.shortURL]  //when inputting url from browser it must start with http:// for it to redirect properly
+  res.redirect(longURL);        //shortURL is inside req.param
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  let shortURL = generateRandomString;
-  res.send(`/urls/:${shortURL}`); // Respond with 'Ok' (we will replace this) //req.body prints {longURL: 'with whatever is inputted from the browser in the input field'}
-  urlDatabase[shortURL] = req.body.longURL;               
-});                       //It's being parsed into a JS object where longURL is the key; 
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;    //assigning shortURL to the object with the longURL           
+  res.redirect(`/urls/${shortURL}`); //req.body prints {longURL: 'with whatever is inputted from the browser in the input field'}
+  //-> http://localhost:8080/urls/b2xVn2
+});                       //It's being parsed into a JS object where longURL is the key inside request? (with the use of bodyParser); 
+
 
 //routes should be ordered from most specific to least specific
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new");  // requesting to go to /urls/new route and we are going to render the .ejs file urls_new
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+
+app.get("/urls/:shortURL", (req, res) => {    //after clicking submit app.post triggers and then you are redirected to a new page here that requests html and you can see the html at urls_show.ejs
+//->http://localhost:8080/urls/b2xVn2
+// ->req.params.shortURL = b2xVn2
+// we want shortURL + longURL, but we only have shortURL, which is -> b2xVn2
+// to find longURL, we are looking it up from our database. which is -> urlDatabase[b2xVn2]
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
+  //reason why we put these two into an object -> because we need to pass it to the ejs file as an object so the file can access it
+  
+  res.render("urls_show", templateVars); //http://localhost:8080/urls/7017cf on this page you want to render the longURL and shortURL so that is why you define it w/TemplateVars
 });
 
 app.listen(PORT, () => {
@@ -56,7 +70,7 @@ app.listen(PORT, () => {
 });
 
 
-//adding additional endpoints
+//adding additional endpoints - reason we are doing this is to translate and exporting out database in HTML 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
